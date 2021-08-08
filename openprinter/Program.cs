eda8137b637a8557f62af5fbbc8f88a06090ee82
lavahasif <lavahasif@gmail.com>
2021-08-08 21:57:25 +0530

    Initial commit

---
+++ openprinter/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+
+namespace openprinter
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+             // byte[] buffer = new byte[5]
+             // {
+             //     (byte) 27,
+             //     (byte) 112,
+             //     (byte) 0,
+             //     (byte) 25,
+             //     (byte) 250
+             // };
+//             Console.Write(buffer);
+//             string author = "Mahesh Chand";
+// // Convert a C# string to a byte array  
+//             byte[] bytes = Encoding.ASCII.GetBytes(author);
+//             foreach (byte b in bytes)
+//             {
+//                 Console.WriteLine(b);
+//             }
+//
+//             // SendBytesToLocalPrinter(buffer, "test");
+//             // Thread.Sleep(2000);
+            const string ESC = "\u001B";
+            const string p = "\u0070";
+            const string m = "\u0000";
+            const string t1 = "\u0025";
+            const string t2 = "\u0250";
+            const string openTillCommand = ESC + p + m + t1 + t2;
+
+            RawPrinterHelper.SendStringToPrinter("Bill", openTillCommand);
+        }
+
+        public static void SendBytesToLocalPrinter(byte[] data, string printerName)
+        {
+            var size = Marshal.SizeOf(data[0]) * data.Length;
+            var pBytes = Marshal.AllocHGlobal(size);
+            try
+            {
+                RawPrinterHelper.SendBytesToPrinter(printerName, pBytes, size);
+            }
+            finally
+            {
+                Marshal.FreeCoTaskMem(pBytes);
+            }
+        }
+    }
+}
---
+++ openprinter/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("openprinter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("openprinter")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("25E45059-7329-4176-9166-CE6E9F79CB7E")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
